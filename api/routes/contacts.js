f9cecd0a31ec70de31d68b8c8efdf54984047a3e
Binod Shrestha <shresthabinod.softdev@gmail.com>
2021-03-26 11:49:02 -0400

    first commit

--- api/routes/contacts.js
+++ api/routes/contacts.js
@@ -8,6 +8,9 @@ router.get('/', (req,res,next) =>{
 });
 
 router.post('/', (req, res, next) =>{
+    const contact = {
+        name: req.body.name
+    }
     res.status(200).json({
         message: 'Handling POST requests to /contacts'
     });
